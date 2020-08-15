c2eb8c54af874275e18a30062406b47340770054
Leonardo Ramos <leorrc96@gmail.com>
2020-08-15 17:09:00 -0400

    Se realizo la logica de las rutas GET para obtener restaurantes de la db

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const db = require('./db/index');
+require("dotenv").config();
+const express = require("express");
+const db = require("./db/index");
 const app = express();
 
 // Este middleware permite obtener el body de "req" en forma de objeto.
@@ -8,52 +8,64 @@ const app = express();
 app.use(express.json());
 
 // Get all Restaurants
-app.get('/api/v1/restaurants', async (req, res) => {
-  const {rows: restaurants} = await db.query('SELECT * FROM restaurants');
+app.get("/api/v1/restaurants", async (req, res) => {
+  try {
+    const { rows: restaurants } = await db.query("SELECT * FROM restaurants");
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      restaurants
-    }
-  })
-})
+    res.status(200).json({
+      status: "success",
+      results: restaurants.length,
+      data: {
+        restaurants,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 // Get a Restaurant
-app.get('/api/v1/restaurants/:id', (req, res) => {
-  console.log(req.params);
-  res.status(200).json({
-    status: 'success',
-    data: 'mcdonalds'
-  })
-})
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const {rows} = await db.query('SELECT * FROM restaurants WHERE  id = $1', [req.params.id]);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: rows[0]
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 // Create Restaurant
-app.post('/api/v1/restaurants/', (req, res) => {
+app.post("/api/v1/restaurants/", (req, res) => {
   console.log(req.body);
   res.status(200).json({
-    status: 'success',
-    data: 'pomodoro'
-  })
-})
+    status: "success",
+    data: "pomodoro",
+  });
+});
 
 // Update restaurant
-app.put('/api/v1/restaurants/:id', (req, res) => {
+app.put("/api/v1/restaurants/:id", (req, res) => {
   console.log(req.params.id);
   console.log(req.body);
   res.status(200).json({
-    status: 'success'
-  })
-})
+    status: "success",
+  });
+});
 
 // Delete Restaurant
-app.delete('/api/v1/restaurants/:id', (req, res) => {
+app.delete("/api/v1/restaurants/:id", (req, res) => {
   res.status(204).json({
-    status: 'success'
-  })
-})
+    status: "success",
+  });
+});
 
 const port = process.env.PORT || 3005;
 app.listen(port, () => {
   console.log(`Server listen on port ${port}`);
-})
+});
